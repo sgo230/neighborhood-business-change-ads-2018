9baade8d3f3459a0b1594b7bc30c326c22454f4e
sgo230 <sgo230@nyu.edu>
2018-12-16 16:08:14 -0500

    Add Yelp API scripts

---
+++ zipList.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+East_New_York = ['11207', '11208']
+Williamsburg = ['11206', '11221', '11237']
+East_Harlem = ['10029', '10035']
+East_Flatbush = ['11203', '11210', '11225', '11226']
+Bensonhurst = ['11204']
+Sheepshead_Bay = ['11235'] # problematic
+Canarsie = ['11236', '11239']
+South_Ozone_Park = ['11436', '11420']
+Murray_Hill = ['10016']
+East_Village = ['10009','10003']
+Midwood = ['11230']
+Gravesend = ['11223']
+Bath_Beach = ['11214']
+Ozone_Park = ['11416','11417']
+Richmond_Hill = ['11418']
+Maspeth = ['11378']
+Bayside = ['11364','11361','11360','11359']
+Chelsea = ['10001', '10018', '10020', '10036']
+Coney_Island = ['11224']
+Parkchester = ['10462','10461']
+Woodhaven = ['11421']
+Fort_Greene = ['11205']
+Dyker_Heights = ['11228']
+Hollis = ['11423']
+Throggs_Neck = ['10465']
+Middle_Village = ['11379']
+#Homecrest = ['11223'] - removed
+Bellerose =['11426']
+#Brighton Beach =['11235'] - removed
+Auburndale = ['11358']
+Howard_Beach = ['11414']
+Flatiron_District = ['10010', '10011']
+#Marine Park = ['11234'] # problematic, delete
+Douglaston_Little_Neck = ['11363']
+Great_Kills = ['10306']
+Fresh_Meadows = ['11366', '11365']
+Eltingville = ['10308']
+West_Village = ['10014']
+Heartland_Village = ['10314']
+West_Brighton = ['14623']
+Tribeca = ['10013','10007']
+Turtle_Bay = ['10022']
+#Midtown = ['10019','10017'] # removed
+Tudor_City = ['10017']
+Arden_Heights = ['10910']
+Jamaica_Estates = ['11432']
+Emerson_Hill = ['10304',  '10301']
+Mill_Basin = ['11234']
+Rosebank = ['10305']
+Rossville = ['10309', '10312']
+SoHo = ['10012']
+Georgetown = ['13072'] # check 
+
+
+# In[8]:
+
+
+N_names = ['East New York', 'Williamsburg', 'East Harlem', 'East Flatbush','Bensonhurst','Sheepshead Bay','Canarsie','South Ozone Park','Murray Hill','East Village','Midwood','Gravesend','Bath Beach','Ozone Park','Richmond Hill','Maspeth','Bayside','Chelsea','Coney Island','Parkchester','Woodhaven','Fort Greene','Dyker Heights','Hollis','Throggs Neck','Middle Village','Bellerose','Auburndale','Howard Beach','Flatiron District','Douglaston-Little Neck','Great Kills','Fresh Meadows','Eltingville','West Village','Heartland Village','West Brighton','Tribeca','Turtle Bay','Tudor City','Arden Heights','Jamaica Estates','Emerson Hill','Mill Basin','Rosebank','Rossville','SoHo','Georgetown']
+
+N_zips = [East_New_York, Williamsburg , East_Harlem, East_Flatbush, Bensonhurst, Sheepshead_Bay, Canarsie, South_Ozone_Park ,Murray_Hill,East_Village , Midwood, Gravesend, Bath_Beach, Ozone_Park, Richmond_Hill, Maspeth , Bayside, Chelsea, Coney_Island, Parkchester, Woodhaven, Fort_Greene,Dyker_Heights,Hollis,Throggs_Neck,Middle_Village,Bellerose,Auburndale,Howard_Beach,Flatiron_District,Douglaston_Little_Neck,Great_Kills,Fresh_Meadows,Eltingville,West_Village,Heartland_Village,West_Brighton,Tribeca,Turtle_Bay,Tudor_City,Arden_Heights,Jamaica_Estates,Emerson_Hill,Mill_Basin,Rosebank,Rossville,SoHo,Georgetown]
+
+
+# In[ ]:
+
+
+# # sources:
+# https://www.zipmap.net/New_York/New_York_County
+# https://www.zip-codes.com/city/
+# http://www.brooklynproperty.com/Brooklyn/ZipCodes.htm
+# https://www.addresses.com/zip-code-lookup/new-york/parkchester
+# https://explorebk.com/brooklyn-zip-codes-neighborhood/
+
